e46934999efeeff0a3d527e90189d432bc796ed4
Jon Ko <jon.ko@gmx.net>
2020-11-14 15:04:43 +0100

    Save and load Stones

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,44 @@
 const express = require('Express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+const Stone = require('./models/stone');
 
 const app = express();
 const port = 3000;
 
+app.use(logger('dev'));
+app.use(express.json());
 app.use(cors({ origin: true })); // TODO: Is this secure?
 
-app.get('/', (req, res) => {
-  res.json([
-      { x: 3, y: 4, team: 'black' },
-      { x: 7, y: 7, team: 'white' }
-  ]);
+app.get('/', async (req, res) => {
+  const stones = await Stone.find();
+  res.json(stones);
+});
+
+app.post('/', async (req, res, next) => {
+  try {
+    const stone = new Stone(req.body);
+    await stone.save();
+    res.status(200).json(stone);
+  } catch (err) {
+    if (err.errors && err.message) {
+      // Probably a validation error by mongoose and the client's fault
+      res.status(400).send(err.message);
+    } else {
+      throw(err);
+    }
+  }
+});
+
+mongoose.connect('mongodb://localhost/mpgo',
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connection opened');
 });
 
 app.listen(port, () => {
---
+++ backend/models/stone.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const StoneSchema = new mongoose.Schema({
+    x: { type: Number, required: true },
+    y: { type: Number, required: true },
+    team: { type: String, required: true, maxlength: 20 }
+});
+
+module.exports = mongoose.model('Stone', StoneSchema);
