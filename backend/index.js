8f6e588b5d03bf8e4e1800af2dc7419e218104b1
Jon Ko <jon.ko@gmx.net>
2020-11-14 15:12:49 +0100

    Format code prettier

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
-const express = require('Express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const logger = require('morgan');
+const express = require("Express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const Stone = require('./models/stone');
+const Stone = require("./models/stone");
 
 const app = express();
 const port = 3000;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(cors({ origin: true })); // TODO: Is this secure?
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   const stones = await Stone.find();
   res.json(stones);
 });
 
-app.post('/', async (req, res, next) => {
+app.post("/", async (req, res, next) => {
   try {
     const stone = new Stone(req.body);
     await stone.save();
@@ -27,18 +27,21 @@ app.post('/', async (req, res, next) => {
       // Probably a validation error by mongoose and the client's fault
       res.status(400).send(err.message);
     } else {
-      throw(err);
+      throw err;
     }
   }
 });
 
-mongoose.connect('mongodb://localhost/mpgo',
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-);
+mongoose.connect("mongodb://localhost/mpgo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('MongoDB connection opened');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connection opened");
 });
 
 app.listen(port, () => {
--- backend/models/stone.js
+++ backend/models/stone.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const StoneSchema = new mongoose.Schema({
-    x: { type: Number, required: true },
-    y: { type: Number, required: true },
-    team: { type: String, required: true, maxlength: 20 }
+  x: { type: Number, required: true },
+  y: { type: Number, required: true },
+  team: { type: String, required: true, maxlength: 20 },
 });
 
-module.exports = mongoose.model('Stone', StoneSchema);
+module.exports = mongoose.model("Stone", StoneSchema);
