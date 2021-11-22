ab3c26be49f168bb82a9d71060d9492f4650daa6
Jon Ko <jon.ko@gmx.net>
2021-11-22 14:05:39 +0100

    Show smaller color indicator for user color
    Users may use different colors for their games. Therefore the user's
    default color shouldn't be shown that prominently. Instead of the big
    indicator and the form to change the colors just a small indicator and
    a button to open the form are shown.

--- vite-pinia-frontend/src/components/UserInfo.vue
+++ vite-pinia-frontend/src/components/UserInfo.vue
@@ -7,42 +7,63 @@
   const store = useStore();
   const { user } = storeToRefs(store);
 
+  const changeColors = ref(false);
   const changedColor = ref("");
   const changedSecondaryColor = ref("");
 
-  store.$subscribe((mutation, state) => {
-    changedColor.value = state.user ? state.user.color : "";
-    changedSecondaryColor.value = state.user ? state.user.secondaryColor : "";
-  });
+  const initializeColors = () => {
+    changedColor.value = store.user ? store.user.color : "";
+    changedSecondaryColor.value = store.user ? store.user.secondaryColor : "";
+  };
 
-  const setColor = () =>
-    store.setColor(changedColor.value, changedSecondaryColor.value);
+  const setColor = async () => {
+    await store.setColor(changedColor.value, changedSecondaryColor.value);
+    changeColors.value = false;
+  };
+
+  const cancelSetColor = () => {
+    initializeColors();
+    changeColors.value = false;
+  };
+
   const logout = () => store.logout();
+
+  store.$subscribe(initializeColors);
 </script>
 
 <template>
   <div v-if="user">
     <!-- TODO: Use player.name or change text -->
-    <span>Playing as {{ user.name }}</span>
-    <button type="button" v-on:click="logout()">Logout</button>
-    <br />
-    <label>
-      <span>Primary Color:</span>
-      <input type="color" v-model="changedColor" v-on:change="setColor()" />
-    </label>
-    <label>
-      <span>Secondary Color:</span>
-      <input
-        type="color"
-        v-model="changedSecondaryColor"
-        v-on:change="setColor()"
+    <span>
+      Logged in as {{ user.name }}
+      <ColorIndicator
+        v-bind:color="user.color"
+        v-bind:secondaryColor="user.secondaryColor"
+        size="25"
       />
-    </label>
-    <br />
-    <ColorIndicator
-      v-bind:color="user.color"
-      v-bind:secondaryColor="user.secondaryColor"
-      size="50"
-    />
+    </span>
+    <button type="button" v-on:click="changeColors = !changeColors">
+      {{ changeColors ? 'Hide "Change colors"' : "Change colors" }}
+    </button>
+    <button type="button" v-on:click="logout()">Logout</button>
+
+    <div v-if="changeColors">
+      <label>
+        <span>Primary Color:</span>
+        <input type="color" v-model="changedColor" />
+      </label>
+      <label>
+        <span>Secondary Color:</span>
+        <input type="color" v-model="changedSecondaryColor" />
+      </label>
+      <br />
+      <ColorIndicator
+        v-bind:color="changedColor"
+        v-bind:secondaryColor="changedSecondaryColor"
+        size="50"
+      />
+      <button type="button" v-on:click="setColor()">Save</button>
+      <button type="button" v-on:click="cancelSetColor()">Cancel</button>
+    </div>
   </div>
 </template>
